0150924121cd696a228ef1e1e681a5c234ae1c51
jadya <jade.saareke@helsinki.fi>
2017-08-08 23:05:01 +0300

    Update Muistipeli

--- Muistipeli/src/main/java/muistipeli/gui/Kayttoliittyma.java
+++ Muistipeli/src/main/java/muistipeli/gui/Kayttoliittyma.java
@@ -1,6 +1,23 @@
 
 package muistipeli.gui;
 
-public class Kayttoliittyma {
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+
+public class Kayttoliittyma implements Runnable {
+    
+    private JFrame frame;
+    
+    public Kayttoliittyma() {
+        
+    }
+
+    @Override
+    public void run() {
+        this.frame = new JFrame("Muistipeli");
+        
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        
+    }
     
 }
