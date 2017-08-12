8ab996a36068472f2b80c7e322454c07cf7bfc6e
jadya <jade.saareke@helsinki.fi>
2017-08-12 17:37:44 +0300

    Update Muistipeli

--- Muistipeli/src/main/java/muistipeli/gui/Kayttoliittyma.java
+++ Muistipeli/src/main/java/muistipeli/gui/Kayttoliittyma.java
@@ -1,14 +1,26 @@
 
 package muistipeli.gui;
 
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
 import java.util.ArrayList;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.WindowConstants;
+import muistipeli.logiikka.Peli;
 
 public class Kayttoliittyma implements Runnable {
     
     private JFrame frame;
-    private ArrayList<Korttipaikka> korttipaikat = new ArrayList<>();
+    private ArrayList<Korttipaikka> korttipaikat;
+    private Peli peli;
+    
+    public Kayttoliittyma() {
+        this.korttipaikat = new ArrayList<>();
+        
+    }
     
     @Override
     public void run() {
@@ -16,6 +28,27 @@ public class Kayttoliittyma implements Runnable {
         
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         
+    }
+    
+    public void rakennaPelinakyma(int leveys, int korkeus) {
+        this.frame.add(new JLabel("Muistipeli"), BorderLayout.NORTH);
+        this.frame.add(new JButton("Lopeta peli"), BorderLayout.EAST);
+        JPanel panel = new JPanel();
+        panel.setLayout(new GridLayout(leveys, korkeus));
+        for(int i = 0 ; i < leveys*korkeus ; i++) {
+            Korttipaikka korttipaikka = new Korttipaikka(null, this.peli.getPelialusta());
+            korttipaikat.add(korttipaikka);
+            panel.add(korttipaikka);
+        }
+        this.frame.add(panel,BorderLayout.CENTER);
+    }
+    
+    public void setPeli(Peli peli) {
+        this.peli = peli;
+    }
+    
+    public Peli getPeli() {
+        return this.peli;
     }
     
 }
